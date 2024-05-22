c36591525aa7848812b3e3e1bf0d8232d1c418d3
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-05-22 14:09:30 +0200

    refactor: remove unused code

--- src/resource_fula_ordboken/__init__.py
+++ src/resource_fula_ordboken/__init__.py
@@ -3,15 +3,6 @@
 __version__ = "0.1.0"
 
 
-def get_version() -> str:
-    """Get version.
-
-    >>> get_version()
-    '0.1.0'
-    """
-    return __version__
-
-
 def user_agent() -> str:
     """User agent."""
     return f"resource-fula-ordboken/{__version__}"
--- tests/test_version.py
+++
@@ -1,5 +0,0 @@
-import resource_fula_ordboken
-
-
-def test_version() -> None:
-    assert resource_fula_ordboken.get_version() == "0.1.0"
